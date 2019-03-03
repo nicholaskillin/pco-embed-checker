10cafa0151deadc07092847f8525b8c62a6725f9
Nick Killin <nicholaskillin@gmail.com>
2019-03-02 23:36:37 -0500

    Added user validation and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "user should have a name" do
+    user = User.new(email: "example@foo.bar")
+    assert_not user.valid?
+  end
+
+  test "user should have an email address" do
+    user = User.new(name: "Test User")
+    assert_not user.valid?
+  end
+
 end
