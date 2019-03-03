11a052583852af14eb0246d4e3ac3b3442d44514
Nick Killin <nicholaskillin@gmail.com>
2019-03-02 23:43:28 -0500

    test to verify that valid users get created

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  setup do
+    @user = User.new(name: "Test Person", email: "example@foo.bar")
+  end
   
   test "user should have a name" do
     user = User.new(email: "example@foo.bar")
@@ -10,6 +14,12 @@ class UserTest < ActiveSupport::TestCase
   test "user should have an email address" do
     user = User.new(name: "Test User")
     assert_not user.valid?
+  end
+
+  test "valid users are saved" do
+    assert_difference 'User.count', 1 do
+      @user.save
+    end
   end
 
 end
