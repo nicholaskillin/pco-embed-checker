036e88e03b58b7fce9a06a684fad35fbd7a51284
Nick Killin <nicholaskillin@gmail.com>
2019-03-24 10:18:07 -0400

    added some validation and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_022112) do
+ActiveRecord::Schema.define(version: 2019_03_24_141605) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
