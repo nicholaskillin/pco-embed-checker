8a371238db59271262e33101463e9eaa2daf611b
Nick Killin <nicholaskillin@gmail.com>
2019-04-02 21:01:38 -0400

    added controller actions and create association between users and widgets

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,10 +1,23 @@
 class WidgetsController < ApplicationController
 
-  def show
-    @widget = Widget.find(params[:id])
-  end
-
   def index
   end
+  
+  def create
+    @widget = current_user.widgets.build(widget_params)
+    if @widget.save
+      flash[:success] = "New widget created."
+      redirect_to root_url
+    else
+      flash[:danger] = "There was a problem."
+      redirect_to root_url
+    end
+  end
+
+  private
+    
+    def widget_params
+      params.require(:widget).permit(:name, :code)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { email.downcase! }
+  has_many :widgets, dependent: :destroy
 
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
