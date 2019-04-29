357112e6b3c5af6ebb34af40fadf9277b6228a88
Nick Killin <nicholaskillin@gmail.com>
2019-04-29 19:01:25 -0400

    added tests for widget sidebar

--- test/controllers/widgets_controller_test.rb
+++ test/controllers/widgets_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class WidgetsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+    @widget = @user.widgets.first
+  end
+  
+  test "widget index page should have side-nav" do
+    log_in_as(@user)
+    get widgets_path
+    assert_template "widgets/index"
+    assert_select "div#side-nav"
+    assert_select "form"
+  end
+
+  test "widget show page should have side-nav" do
+    log_in_as(@user)
+    get widget_path(@widget)
+    assert_template "widgets/show"
+    assert_select "div#side-nav"
+    assert_select "form"
+  end
+
 end
