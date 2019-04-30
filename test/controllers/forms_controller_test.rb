ac5dda617a4c6fbc850f2ea5a2853d83c0afcb54
Nick Killin <nicholaskillin@gmail.com>
2019-04-29 20:19:24 -0400

    added basic sidebar tests for forms sidebar

--- test/controllers/forms_controller_test.rb
+++ test/controllers/forms_controller_test.rb
@@ -1,5 +1,26 @@
 require 'test_helper'
 
 class FormsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+    @form = @user.forms.first
+  end
+  
+  test "form index page should have side-nav" do
+    log_in_as(@user)
+    get forms_path
+    assert_template "forms/index"
+    assert_select "div#side-nav"
+    assert_select "form"
+  end
+
+  test "forms show page should have side-nav" do
+    log_in_as(@user)
+    get form_path(@form)
+    assert_template "forms/show"
+    assert_select "div#side-nav"
+    assert_select "form"
+  end
  
 end
