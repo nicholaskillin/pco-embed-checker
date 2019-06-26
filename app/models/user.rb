e26c9cd46253e3f5a3c312a4b6729ee29d4daeae
Nick Killin <nicholaskillin@gmail.com>
2019-06-25 22:05:09 -0400

    cleaned up code from testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   def self.find_or_create_from_auth_hash(auth)
 		where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
 			user.provider = auth.provider
-			user.uid = auth.uid
+      user.uid = auth.uid
       user.name = auth.info.name
 			user.save!
 		end
