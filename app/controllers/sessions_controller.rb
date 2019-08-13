6aaae2ed25801f19443c53cea9b215815fc1454e
Kayla Hansen <kayla@pco.bz>
2019-08-13 11:11:34 -0700

    call log out before rendering, delete destroy route, fix indentation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,27 +4,21 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create    
+  def create
     org_id = auth_hash['extra']['org_id']
     if org_id == "1"
       @user = User.find_or_create_from_auth_hash(auth_hash)
       session[:user_id] = @user.id
       redirect_to root_url
     else
-      render 'new'
       log_out
+      render 'new'
     end
-  end
-
-  def destroy
-    log_out
-    redirect_to root_url
   end
 
   private
 
-    def auth_hash
-      request.env['omniauth.auth']
-    end
-
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 end
