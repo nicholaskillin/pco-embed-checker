43f47a8ef1764b3e6cdc53f1f02a3569436d177a
Kayla Hansen <kayla@pco.bz>
2019-08-13 11:10:01 -0700

    remove users paths, remove logout path

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,10 @@ Rails.application.routes.draw do
   get 'donation_forms/index'
   root 'widgets#index'
   get 'sessions/new'
-  get    '/signup',  to: 'users#new'
   get 'login', to: redirect('/auth/planning_center'), as: 'login'
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  post '/login', to: 'sessions#create'
   resources :widgets
   resources :forms
   resources :donation_forms
