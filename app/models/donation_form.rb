34bc7197e05c81c0b0974869810a846ad02babce
Josh Wilkerson <jgwilkerson@gmail.com>
2019-08-13 14:58:36 -0500

    Add “app” to DonationForm model

--- app/models/donation_form.rb
+++ app/models/donation_form.rb
@@ -2,4 +2,6 @@ class DonationForm < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :url, presence: true
+
+  enum app: [:giving, :people, :resources]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_033640) do
+ActiveRecord::Schema.define(version: 2019_08_13_192723) do
 
   create_table "donation_forms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_05_14_033640) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "app"
     t.index ["user_id"], name: "index_donation_forms_on_user_id"
   end
 
