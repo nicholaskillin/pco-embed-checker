e336a93592f71d8f820baf047b7783d2086a6658
Kayla Hansen <kayla@pco.bz>
2019-08-14 09:52:29 -0700

    validate url length

--- app/models/donation_form.rb
+++ app/models/donation_form.rb
@@ -2,6 +2,7 @@ class DonationForm < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :url, presence: true
+  validates :url, length: { maximum: 255 }
 
   enum app: [:giving, :people, :resources]
 end
