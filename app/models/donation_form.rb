1234714e2c18130c6d3e1e666e7b7fd4c2252d82
Kayla Hansen <kayla@pco.bz>
2019-08-15 10:34:59 -0700

    delete donation_forms, create migration to drop table

--- app/models/donation_form.rb
+++
@@ -1,6 +0,0 @@
-class DonationForm < ApplicationRecord
-  belongs_to :user
-  validates :name, presence: true
-  validates :url, presence: true
-  validates :url, length: { maximum: 255 }
-end
--- test/models/donation_form_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DonationFormTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
